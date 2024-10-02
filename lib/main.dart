b3a2be0d2dce3f90c432e3eb3e94f8a2f90475c3
pious <109950629+pious2847@users.noreply.github.com>
2024-10-02 22:59:51 +0000

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doorbel/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
--- lib/services/google_API.dart
+++ lib/services/google_API.dart
@@ -1,14 +1,17 @@
 import 'package:dio/dio.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class GoogleAPI {
-  Future<List<dynamic>>  getResturant() async{
-    final dio Dio();
-    try{
-      final response = await dio.get(); 
-    }catch(e){
-
+  final String? APiUri = dotenv.env['Google_Map_Api'];
+  Future<List<dynamic>> getResturant() async {
+    final dio = Dio();
+    try {
+      final response = await dio.get();
+      if(response.statusCode == 200){
+        return response.data;
+      }
+    } catch (e) {
+      return e;
     }
   }
 }
-
