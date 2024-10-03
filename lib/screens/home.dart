5c15dce0160d82411a7da1a6add89ea57507cb33
pious <109950629+pious2847@users.noreply.github.com>
2024-10-03 13:36:54 +0000

    Updates on IOS permissions

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,20 +1,38 @@
+import 'package:doorbel/services/google_API.dart';
+import 'package:doorbel/widget/ResturantCards.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-
-  const Home({ super.key });
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  final googleApi = GoogleAPI();
+  final ResturantData = [];
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Doorbel'),),
-           body: Container(),
-       );
+  @override
+  void initState() {
+
+    super.initState();
+  }
+  fetchResturants() async {
+    try {
+      
+    } catch (e) {
+      
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Doorbel'),
+      ),
+      body: Container(),
+    );
   }
 }
--- lib/services/google_API.dart
+++ lib/services/google_API.dart
@@ -1,17 +1,42 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
 
 class GoogleAPI {
   final String? APiUri = dotenv.env['Google_Map_Api'];
-  Future<List<dynamic>> getResturant() async {
+  final String? APiKey = dotenv.env['Google_Map_Api'];
+
+  Future<List<dynamic>> getRestaurants() async {
+    print("Env Data $APiUri   :  Key $APiKey");
+    
     final dio = Dio();
+
     try {
-      final response = await dio.get();
-      if(response.statusCode == 200){
-        return response.data;
+      // Get current location
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+
+      // Google Places API endpoint
+      String url =
+          'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+
+      // API parameters
+      Map<String, dynamic> queryParameters = {
+        'location': '${position.latitude},${position.longitude}',
+        'radius': '1500', // Search within 1500 meters
+        'type': 'restaurant',
+        'key': '$APiKey'
+      };
+
+      final response = await dio.get(url, queryParameters: queryParameters);
+
+      if (response.statusCode == 200) {
+        return response.data['results'];
+      } else {
+        throw Exception('Failed to fetch restaurants');
       }
     } catch (e) {
-      return e;
+      throw Exception('Failed to make API call: $e');
     }
   }
 }
